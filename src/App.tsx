ab6177872dee863030325c6a2df1aa2c2a79983d
elegant <1258291382@qq.com>
2025-03-04 12:49:44 +0800

    visitor

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Resume from './components/Resume'
+import VisitorStats from './components/VisitorStats'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Resume />
+      <VisitorStats />
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
